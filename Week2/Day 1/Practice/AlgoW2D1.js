a26dbdd0cb908d40ec35a1afe09aa52ae317f5d4
Hichem CHERIF <hichemcherif47@gmail.com>
2023-05-08 10:17:24 +0100

    Fizzbuzz

---
+++ Week2/Day 1/Practice/AlgoW2D1.js
@@ -0,0 +1,13 @@
+var inverse="";
+function reverse(arr){
+    var start=0;
+    while (start<arr.length-start){
+        inverse=arr[arr.length-1-start];
+        arr[arr.length-1-start]=arr[start];
+        arr[start]=inverse;
+        start++;
+    }
+    return arr;
+}
+console.log(reverse(["a","b","c","d","e"]));
+console.log(reverse([1,2,3,4]));
