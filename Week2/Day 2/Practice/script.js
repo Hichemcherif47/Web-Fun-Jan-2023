a2cd271fb16f1087848444b077bced47d33bc8fc
Hichem CHERIF <hichemcherif47@gmail.com>
2023-05-09 12:45:41 +0100

    Video Preview

---
+++ Week2/Day 2/Practice/script.js
@@ -0,0 +1,13 @@
+console.log("page loaded...");
+
+function playVideo(video){
+    video.play();
+}
+
+function pauseVideo(video){
+    video.pause();
+}
+
+function mutVideo(vid){
+    vid.muted();
+}
