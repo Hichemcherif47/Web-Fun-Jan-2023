f2d4cc91c68557cc9040ca8769b0b2b2b78346dc
Hichem CHERIF <hichemcherif47@gmail.com>
2023-05-09 17:51:20 +0100

    Liking Likes

---
+++ Week2/Day 2/Core/Liking Likes part I.js
@@ -0,0 +1,6 @@
+var nblike=document.querySelector('#nblike');
+var nbr=3;
+function moreLikes(){
+    nbr++;
+    nblike.innerHTML=nbr + ' like(s)';
+}
---
+++ Week2/Day 2/Core/Liking Likes part II.js
@@ -0,0 +1,20 @@
+var nblike1=document.querySelector('#nblike1');
+var nbr1=9;
+function moreLikes1(){
+    nbr1++;
+    nblike1.innerHTML=nbr1 + ' like(s)';
+}
+
+var nblike2=document.querySelector('#nblike2');
+var nbr2=12;
+function moreLikes2(){
+    nbr2++;
+    nblike2.innerHTML=nbr2 + ' like(s)';
+}
+
+var nblike3=document.querySelector('#nblike3');
+var nbr3=9;
+function moreLikes3(){
+    nbr3++;
+    nblike3.innerHTML=nbr3 + ' like(s)';
+}
